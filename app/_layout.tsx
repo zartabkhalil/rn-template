6abcf005c43ab05ab54138c383dd9a6b9b8e66a5
bug_hunter <76158483+zartabkhalil@users.noreply.github.com>
2026-06-22 13:50:02 +0500

    fix font import issue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import FontAssets from '@/constants/fontAssets';
+import { FontAssets } from '@/constants/fontAssets';
 
 export default function RootLayout() {
   const [loaded] = useFonts(FontAssets);
